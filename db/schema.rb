dd5ee2e8ea02539657a81e83dd532681ea08da11
Martino Giacchetti <martino.giacchetti@gmail.com>
2016-10-25 17:57:49 +0200

    Create through scaffold picutres (it will contain a paint)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024154155) do
+ActiveRecord::Schema.define(version: 20161025155642) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
