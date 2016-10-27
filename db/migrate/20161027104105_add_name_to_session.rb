7443c45de5eaf3ce2b2d58892b86ba93932619d3
Martino Giacchetti <martino.giacchetti@gmail.com>
2016-10-27 12:48:50 +0200

    created migration to add name to session

---
+++ db/migrate/20161027104105_add_name_to_session.rb
@@ -0,0 +1,5 @@
+class AddNameToSession < ActiveRecord::Migration
+  def change
+    add_column :sessions, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027084503) do
+ActiveRecord::Schema.define(version: 20161027104105) do
 
   create_table "musics", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20161027084503) do
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
