c04621fd38d4ac758a38b54a1fa82fbba86b554f
Martino Giacchetti <martino.giacchetti@gmail.com>
2016-11-15 16:10:34 +0100

    runned migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031180826) do
+ActiveRecord::Schema.define(version: 20161114173255) do
+
+  create_table "finalphases", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "musics", force: :cascade do |t|
     t.string   "name"
